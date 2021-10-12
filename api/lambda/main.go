2f8359cea433368dfa89e3fa9aec673a7c6569e9
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2021-10-12 19:23:13 +0000

    this should work

--- api/lambda/main.go
+++ api/lambda/main.go
@@ -6,4 +6,5 @@ import (
 
 	"github.com/aws/aws-lambda-go/lambda"
 	logger "github.com/lenoobz/aws-lambda-logger"
-	"github.com/lenoobz/aws-yahoo-asset-price-scraper/c
+	"github.com/lenoobz/aws-yahoo-asset-price-scraper/config"
+	"github.com/lenoobz/aws-yahoo-asset-pr
