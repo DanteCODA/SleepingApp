231b8f0276ef91e7c9251cb26d5ba010fc6b40cf
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2021-10-16 06:30:52 +0000

    moved to src

--- api/lambda/main.go
+++ api/lambda/main.go
@@ -7,4 +7,5 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	logger "github.com/lenoobz/aws-lambda-logger"
 	"github.com/lenoobz/aws-yahoo-asset-price-scraper/config"
-	"github.com/lenoobz/aws-yahoo-asset-pr
+	"github.com/lenoobz/aws-yahoo-asset-price-scraper/consts"
+	"github.com/lenoobz/aws-yahoo-asset-price-
