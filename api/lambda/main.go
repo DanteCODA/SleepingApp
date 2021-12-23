7944dec814289d00f86ad2654b338cab7065d719
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2021-12-23 07:54:08 +0000

    Updated installation script

--- api/lambda/main.go
+++ api/lambda/main.go
@@ -55,4 +55,4 @@ func lambdaHandler(ctx context.Context) {
 	// create new services
 	checkpointService := checkpoint.NewService(checkpointRepo, zap)
 	assetService := assets.NewService(assetRepo, *checkpointService, zap)
-	pric
+	priceService := price.NewService(assetPriceRepo, zap
