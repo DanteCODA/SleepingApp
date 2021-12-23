b35650f307e579a66413d6630fa3e3968448d179
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2021-12-23 22:56:09 +0000

    Remove unused flag

--- config/config.helper.go
+++ config/config.helper.go
@@ -1,2 +1,17 @@
 
 package config
+
+import (
+	"fmt"
+)
+
+// AllowDomain const
+const AllowDomain = "ca.finance.yahoo.com"
+
+// DomainGlob const
+const DomainGlob = "*yahoo.*"
+
+// GetPriceByTickerURL get price url
+func GetPriceByTickerURL(ticker string) string {
+	return fmt.Sprintf("https://ca.finance.yahoo.com/quote/%s", ticker)
+}
