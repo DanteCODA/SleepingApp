ff0d6e9eb9a98a04f5269ea7a1ffcbc9d24494f6
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-01-04 22:57:56 +0000

    Created a README

--- config/config_staging.go
+++ config/config_staging.go
@@ -7,3 +7,7 @@ import "os"
 var host = os.Getenv("MONGO_DB_HOST")
 var username = os.Getenv("MONGO_DB_USERNAME")
 var password = os.Getenv("MONGO_DB_PASSWORD")
+
+// AppConf constants
+var AppConf = AppConfig{
+	Mongo:
