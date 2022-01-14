da4130d6d455607d201b36ba52d61acb49e834e6
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-01-14 05:43:50 +0000

    change prefix

--- consts/app.const.go
+++ consts/app.const.go
@@ -3,4 +3,5 @@ package consts
 // Collection names
 const (
 	ASSETS_COLLECTION            = "assets"
-	ASSET_PRICES_COLLECTION    
+	ASSET_PRICES_COLLECTION      = "asset_prices"
+	SCRAPE_CHECKPOINT_COLLECTION 
