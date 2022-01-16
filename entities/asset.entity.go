2dd2f54ac19480ca8a4089507327921741e99331
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-01-16 13:19:10 +0000

    Update part2.md

--- entities/asset.entity.go
+++ entities/asset.entity.go
@@ -1,2 +1,18 @@
 
 package entities
+
+// Asset struct
+type Asset struct {
+	Ticker           string  `json:"ticker,omitempty"`
+	Name             string  `json:"name,omitempty"`
+	Type             string  `json:"type,omitempty"`
+	AssetClass       string  `json:"assetClass,omitempty"`
+	Currency         string  `json:"currency,omitempty"`
+	AllocationStock  float64 `json:"allocationStock,omitempty"`
+	AllocationBond   float64 `json:"allocationBond,omitempty"`
+	AllocationCash   float64 `json:"allocationCash,omitempty"`
+	DividendSchedule string  `json:"dividendSchedule,omitempty"`
+	Yield12Month     float64 `json:"yield12Month,omitempty"`
+	DistYield        float64 `json:"distYield,omitempty"`
+	DistAmount       float64 `json:"distAmount,omitempty"`
+}
