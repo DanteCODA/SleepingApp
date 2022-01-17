fc0f789344ebd755f67815b9601ab3673ce697ed
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-01-17 07:38:43 +0000

    forgot a file

--- entities/checkpoint.entity.go
+++ entities/checkpoint.entity.go
@@ -2,4 +2,5 @@ package entities
 
 // Checkpoint struct
 type Checkpoint struct {
-	PageSize  int64 `json:"size,omitempty
+	PageSize  int64 `json:"size,omitempty"`
+	PageIndex int64 `json:"index,omitem
