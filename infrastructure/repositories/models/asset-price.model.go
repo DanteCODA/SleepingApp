0b59051ef56b59c782560257e13f6be5099569e2
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-07-01 09:14:01 +0000

    Delete workspace.xml

--- infrastructure/repositories/models/asset-price.model.go
+++ infrastructure/repositories/models/asset-price.model.go
@@ -24,4 +24,4 @@ type AssetPriceModel struct {
 }
 
 // NewAssetPriceModel create asset price model
-func NewAssetPriceModel(ctx context.Context, log logger.ContextLog, assetPrice *entities.AssetP
+func NewAssetPriceModel(ctx context.Context, log logger.ContextLog, assetPrice *entities.AssetPrice, schemaVersion string) (*AssetPriceModel, err
