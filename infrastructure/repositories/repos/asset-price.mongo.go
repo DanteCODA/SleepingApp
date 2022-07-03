743b3bff97d2a6b13a7450648cc637e2166b84cc
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-07-03 14:16:08 +0000

    Changes to gitignore

--- infrastructure/repositories/repos/asset-price.mongo.go
+++ infrastructure/repositories/repos/asset-price.mongo.go
@@ -25,3 +25,43 @@ type AssetPriceMongo struct {
 }
 
 // NewAssetPriceMongo creates new asset price mongo repo
+func NewAssetPriceMongo(db *mongo.Database, log logger.ContextLog, conf *config.MongoConfig) (*AssetPriceMongo, error) {
+	if db != nil {
+		return &AssetPriceMongo{
+			db:   db,
+			log:  log,
+			conf: conf,
+		}, nil
+	}
+
+	// set context with timeout from the config
+	// create new context for the query
+	ctx, cancel := createContext(context.Background(), conf.TimeoutMS)
+	defer cancel()
+
+	// set mongo client options
+	clientOptions := options.Client()
+
+	// set min pool size
+	if conf.MinPoolSize > 0 {
+		clientOptions.SetMinPoolSize(conf.MinPoolSize)
+	}
+
+	// set max pool size
+	if conf.MaxPoolSize > 0 {
+		clientOptions.SetMaxPoolSize(conf.MaxPoolSize)
+	}
+
+	// set max idle time ms
+	if conf.MaxIdleTimeMS > 0 {
+		clientOptions.SetMaxConnIdleTime(time.Duration(conf.MaxIdleTimeMS) * time.Millisecond)
+	}
+
+	// construct a connection string from mongo config object
+	cxnString := fmt.Sprintf("mongodb+srv://%s:%s@%s", conf.Username, conf.Password, conf.Host)
+
+	// create mongo client by making new connection
+	client, err := mongo.Connect(ctx, clientOptions.ApplyURI(cxnString))
+	if err != nil {
+		return nil, err
+	}
