495e3090b73c94fa0a8e1a1ddc171f6be161294b
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-07-05 08:53:09 +0000

    kek

--- infrastructure/repositories/repos/asset.mongo.go
+++ infrastructure/repositories/repos/asset.mongo.go
@@ -25,3 +25,45 @@ type AssetMongo struct {
 
 // NewAssetMongo creates new asset mongo repo
 func NewAssetMongo(db *mongo.Database, log logger.ContextLog, conf *config.MongoConfig) (*AssetMongo, error) {
+	if db != nil {
+		return &AssetMongo{
+			db:   db,
+			log:  log,
+			conf: conf,
+		}, nil
+	}
+
+	// set context with timeout from the config
+	// create new context for the query
+	ctx, cancel := createContext(context.Background(), conf.TimeoutMS)
+	defer cancel()
+
+	// set mongo client options
+	clientOptions := options.Client()
+
+	// set min pool size
+	if conf.MinPoolSize > 0 {
+		clientOptions.SetMinPoolSize(conf.MinPoolSize)
+	}
+
+	// set max pool size
+	if conf.MaxPoolSize > 0 {
+		clientOptions.SetMaxPoolSize(conf.MaxPoolSize)
+	}
+
+	// set max idle time ms
+	if conf.MaxIdleTimeMS > 0 {
+		clientOptions.SetMaxConnIdleTime(time.Duration(conf.MaxIdleTimeMS) * time.Millisecond)
+	}
+
+	// construct a connection string from mongo config object
+	cxnString := fmt.Sprintf("mongodb+srv://%s:%s@%s", conf.Username, conf.Password, conf.Host)
+
+	// create mongo client by making new connection
+	client, err := mongo.Connect(ctx, clientOptions.ApplyURI(cxnString))
+	if err != nil {
+		return nil, err
+	}
+
+	return &AssetMongo{
+		db:     client.Database(conf.Dbname),
