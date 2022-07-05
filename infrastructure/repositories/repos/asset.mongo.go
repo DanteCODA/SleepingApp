891aa90b32b8cad866f973d88c3c951849f7cf99
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-07-05 14:56:05 +0000

    Build dist

--- infrastructure/repositories/repos/asset.mongo.go
+++ infrastructure/repositories/repos/asset.mongo.go
@@ -172,3 +172,48 @@ func (r *AssetMongo) FindAllAssets(ctx context.Context) ([]*entities.Asset, erro
 
 	return assets, nil
 }
+
+// FindAssetsFromCheckpoint find assets from checkpoint
+func (r *AssetMongo) FindAssetsFromCheckpoint(ctx context.Context, checkpoint *entities.Checkpoint) ([]*entities.Asset, error) {
+	// create new context for the query
+	ctx, cancel := createContext(ctx, r.conf.TimeoutMS)
+	defer cancel()
+
+	// what collection we are going to use
+	colname, ok := r.conf.Colnames[consts.ASSETS_COLLECTION]
+	if !ok {
+		r.log.Error(ctx, "cannot find collection name")
+		return nil, fmt.Errorf("cannot find collection name")
+	}
+	col := r.db.Collection(colname)
+
+	// filter
+	filter := bson.D{}
+
+	// find options
+	findOptions := options.Find().SetSort(bson.D{{Key: "_id", Value: -1}}).SetSkip(checkpoint.PageIndex * checkpoint.PageSize).SetLimit(checkpoint.PageSize)
+
+	cur, err := col.Find(ctx, filter, findOptions)
+
+	// only run defer function when find success
+	if cur != nil {
+		defer func() {
+			if deferErr := cur.Close(ctx); deferErr != nil {
+				err = deferErr
+			}
+		}()
+	}
+
+	// find was not succeed
+	if err != nil {
+		r.log.Error(ctx, "find query failed", "error", err)
+		return nil, err
+	}
+
+	var assets []*entities.Asset
+
+	// iterate over the cursor to decode document one at a time
+	for cur.Next(ctx) {
+		// decode cursor to activity model
+		var asset entities.Asset
+		if err = cur.Decode(&asset); err != nil {
