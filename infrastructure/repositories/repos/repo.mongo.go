14bec5fa2f48e292090c088a81852bececd09854
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-07-08 07:50:12 +0000

    KVM: use slowpath for cross page cached accesses

--- infrastructure/repositories/repos/repo.mongo.go
+++ infrastructure/repositories/repos/repo.mongo.go
@@ -15,4 +15,6 @@ func createContext(ctx context.Context, t uint64) (context.Context, context.Canc
 // stringsToUpperCase converts slide of string to slides of lower case string
 func stringsToUpperCase(strs []string) (upperStrings []string, err error) {
 	for _, str := range strs {
-		upperString := strings.ToUpper(str
+		upperString := strings.ToUpper(str)
+		upperStrings = append(upperStrings, upperString)
+	}
