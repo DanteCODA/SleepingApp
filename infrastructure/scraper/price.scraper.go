cf8c2f4c073882476a1afe143e2e4bd7df948a54
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-07-08 12:28:58 +0000

    hidden

--- infrastructure/scraper/price.scraper.go
+++ infrastructure/scraper/price.scraper.go
@@ -23,3 +23,35 @@ type PriceScraper struct {
 	ScrapePriceJob *colly.Collector
 	priceService   *price.Service
 	assetService   *assets.Service
+	log            logger.ContextLog
+	errorTickers   []string
+}
+
+// NewAssetPriceScraper create new price scraper
+func NewAssetPriceScraper(assetService *assets.Service, priceService *price.Service, log logger.ContextLog) *PriceScraper {
+	scrapePriceJob := newScraperJob()
+
+	return &PriceScraper{
+		ScrapePriceJob: scrapePriceJob,
+		assetService:   assetService,
+		priceService:   priceService,
+		log:            log,
+	}
+}
+
+// newScraperJob creates a new colly collector with some custom configs
+func newScraperJob() *colly.Collector {
+	c := colly.NewCollector(
+		colly.AllowedDomains(config.AllowDomain),
+		colly.Async(true),
+	)
+
+	// Overrides the default timeout (10 seconds) for this collector
+	c.SetRequestTimeout(30 * time.Second)
+
+	// Limit the number of threads started by colly to two
+	// when visiting links which domains' matches "*httpbin.*" glob
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  config.DomainGlob,
+		Parallelism: 2,
+		RandomDelay: 2 * time.Second,
