17d5da944448520fb6868da9caa797f803925d13
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-07-08 13:36:01 +0000

    Updated Makefile

--- infrastructure/scraper/price.scraper.go
+++ infrastructure/scraper/price.scraper.go
@@ -55,3 +55,43 @@ func newScraperJob() *colly.Collector {
 		DomainGlob:  config.DomainGlob,
 		Parallelism: 2,
 		RandomDelay: 2 * time.Second,
+	})
+
+	extensions.RandomUserAgent(c)
+	extensions.Referer(c)
+
+	return c
+}
+
+// configJobs configs on error handler and on response handler for scaper jobs
+func (s *PriceScraper) configJobs() {
+	s.ScrapePriceJob.OnError(s.errorHandler)
+	s.ScrapePriceJob.OnScraped(s.scrapedHandler)
+	s.ScrapePriceJob.OnHTML("div[id=quote-header-info]", s.processPriceResponse)
+}
+
+// ScrapeAllAssetPrices scrape all assets price
+func (s *PriceScraper) ScrapeAllAssetPrices() {
+	ctx := context.Background()
+
+	s.configJobs()
+
+	assets, err := s.assetService.GetAllAssets(ctx)
+	if err != nil {
+		s.log.Error(ctx, "get assets list failed", "error", err)
+	}
+
+	for _, asset := range assets {
+		reqContext := colly.NewContext()
+		reqContext.Put("ticker", asset.Ticker)
+		reqContext.Put("currency", asset.Currency)
+
+		url := config.GetPriceByTickerURL(asset.Ticker)
+
+		s.log.Info(ctx, "scraping asset price", "ticker", asset.Ticker)
+		if err := s.ScrapePriceJob.Request("GET", url, nil, reqContext, nil); err != nil {
+			s.log.Error(ctx, "scraping asset price", "error", err, "ticker", asset.Ticker)
+		}
+	}
+
+	s.ScrapePriceJob.Wait()
