f2759721d5be5c1223b0d4d5c92a4bac060bff92
DanteCODA <107098490+DanteCODA@users.noreply.github.com>
2022-09-15 13:43:57 +0000

    Tidied up code

--- usecase/assets/asset.service.go
+++ usecase/assets/asset.service.go
@@ -4,4 +4,5 @@ import (
 	"context"
 
 	logger "github.com/lenoobz/aws-lambda-logger"
-	"github.com/lenoobz/aws-yahoo-asset-price-scraper/entities
+	"github.com/lenoobz/aws-yahoo-asset-price-scraper/entities"
+	"github.com/lenoobz/aws-yahoo-asset-price-scrape
